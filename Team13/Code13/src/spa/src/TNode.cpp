5f9fb5f632549310dd8fb5dee9c549a090c88298
r4venz <r4venz@yahoo.com>
2020-09-01 00:54:18 +0800

    implement TNode

--- Team13/Code13/src/spa/src/TNode.cpp
+++ Team13/Code13/src/spa/src/TNode.cpp
@@ -1,1 +1,63 @@
 #include "TNode.h"
+
+TNode::TNode(void) {
+
+}
+
+TNode::TNode(NodeType _type, Name _name, Value _value, Operator _op, Index _index, list<TNode*>* _children) 
+	: type(_type), value(_value), name(_name), op(_op), index(_index)
+{
+	if (_children != NULL) 
+	{
+		children = _children;
+	}
+	else 
+	{
+		children = new list<TNode*>;
+	}
+};
+
+bool TNode::AddChild(TNode* child)
+{
+	if (child == NULL) {
+		return false;
+	}
+	(*children).push_back(child);
+	return true;
+};
+
+bool TNode::SetParent(TNode* newParent)
+{
+	if (newParent == NULL) {
+		return false;
+	}
+	parent = newParent;
+	return true;
+};
+
+bool TNode::SetName(string newName)
+{
+	if (type != varName && type != procName) {
+		return false;
+	}
+	name = newName;
+	return true;
+};
+
+bool TNode::SetValue(double newValue)
+{
+	if (type != constValue) {
+		return false;
+	}
+	value = newValue;
+	return true;
+};
+
+bool TNode::SetOp(Operator newOp)
+{
+	if (type != relExpr && type != condExpr && type != expr && type != term) {
+		return false;
+	}
+	op = newOp;
+	return true;
+};
--- Team13/Code13/src/spa/src/TNode.h
+++ Team13/Code13/src/spa/src/TNode.h
@@ -1,5 +1,69 @@
+#include <string>
+#include <list>
+
+using namespace std;
+
+enum NodeType
+{
+	program,
+	procedure,
+	stmtList,
+	readStmt,
+	printStmt,
+	callStmt,
+	whileStmt,
+	ifStmt,
+	assignStmt,
+	condExpr,
+	relExpr,
+	relFactor,
+	expr,
+	term,
+	factor,
+	varName,
+	procName,
+	constValue
+};
+
+enum Operator
+{
+	not,
+	and,
+	or ,
+	greater,
+	geq,
+	lesser,
+	leq,
+	equal,
+	unequal,
+	plus,
+	minus,
+	times,
+	divide,
+	mod
+};
+
+typedef double Value;
+typedef string Name;
+typedef int Index;
+
 class TNode
 {
 public:
+	TNode(void);
+	TNode(NodeType _type, Name _name, Value _value, Operator _op, Index _index, list<TNode*>* _children);
+	bool AddChild(TNode* child);
+	bool SetParent(TNode* newParent);
+	bool SetName(string newName);
+	bool SetValue(double newValue);
+	bool SetOp(Operator newOp);
 
+private:
+	NodeType type;
+	TNode* parent;
+	list<TNode*>* children;
+	Value value;
+	Name name;
+	Operator op;
+	Index index;
 };
