c46771ecae8373762f2c484f3fc27e1064f1820e
chanjunren <chanjunren@gmail.com>
2020-09-18 23:39:12 +0800

    Saving progress

---
+++ Team13/Code13/src/unit_testing/src/frontend/SimplePrograms.h
@@ -0,0 +1,6 @@
+
+#include <string.h>
+
+class SimpleProgram {
+	std::string ifTest1 = "procedure ifStatementTest{ if (x > 5) { call hello; } }"
+}
--- Team13/Code13/src/unit_testing/src/frontend/TestParser.cpp
+++ Team13/Code13/src/unit_testing/src/frontend/TestParser.cpp
@@ -1,10 +1,12 @@
 #include <frontend/Parser.h>
 #include <frontend/Tokenizer.h>
 #include <testUtils/Statement.h>
-
+#include <frontend/CodeExtractor.h>
 #include <catch.hpp>
 
 TEST_CASE("Positive test case: Parsing regular assignment statement") {
-	
+	std::string ifStatement = "procedure ifStatementTest {if (x > 5) {call hello;}}";
+	Parser parser = Parser();
+	parser.parse(ifStatement);
 }
 
