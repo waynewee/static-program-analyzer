649ff5dbd42069a80d16cc2e76cb095167a00980
Woodnsuns <pollylin0617@gmail.com>
2020-09-22 15:46:34 +0800

    adding ExtractPattern unit test

--- Team13/Code13/src/unit_testing/src/TestDesignExtractor.cpp
+++ Team13/Code13/src/unit_testing/src/TestDesignExtractor.cpp
@@ -291,3 +291,37 @@ TEST_CASE("Test Add Modifies") {
     REQUIRE(trueProcModifies);
     REQUIRE(!falseProcModifies);
 }
+
+TEST_CASE("Test extract Pattern") {
+    TNode* prog = new TNode(TNode::NODE_TYPE::program, "SIMPLE");
+    TNode* proc = new TNode(TNode::NODE_TYPE::procedure);
+    TNode* proc_name = new TNode(TNode::NODE_TYPE::procName, "main");
+    TNode* stmt_list = new TNode(TNode::NODE_TYPE::stmtList);
+    TNode* assign1 = new TNode(TNode::NODE_TYPE::assignStmt);
+    TNode* var1 = new TNode(TNode::NODE_TYPE::varName, "var1");
+    TNode* expr1 = new TNode(TNode::NODE_TYPE::expr, TNode::OPERATOR::plus);
+    TNode* var2 = new TNode(TNode::NODE_TYPE::varName, "var2");
+    TNode* const1 = new TNode(TNode::NODE_TYPE::constValue, 5);
+
+    prog->AddChild(proc);
+    proc->AddChild(proc_name);
+    proc->AddChild(stmt_list);
+    stmt_list->AddChild(assign1);
+    assign1->AddChild(var1);
+    assign1->AddChild(expr1);
+    expr1->AddChild(var2);
+    expr1->AddChild(const1);
+    proc->SetParent(prog);
+    proc_name->SetParent(proc);
+    stmt_list->SetParent(proc);
+    assign1->SetParent(stmt_list);
+    var1->SetParent(assign1);
+    expr1->SetParent(assign1);
+    var2->SetParent(expr1);
+    const1->SetParent(expr1);
+
+    PKB* pkb = new PKB();
+    PatternManager pattern_manager = pkb->GetPatternManager();
+    ExtractPattern(pattern_manager, *prog);
+
+}
---
+++ Team13/Code13/src/unit_testing/src/TestPatternManager.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Xu Lin on 22/9/20.
+//
+
