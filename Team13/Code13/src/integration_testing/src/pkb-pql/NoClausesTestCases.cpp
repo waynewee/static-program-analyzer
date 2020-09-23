39bd8dd92b2ae13da949910cc4bb80c12a3f48b0
jiasimin_ <jas.mint98@gmail.com>
2020-09-23 15:09:24 +0800

    Added integration test cases for PKB to PQL.

---
+++ Team13/Code13/src/integration_testing/src/pkb-pql/NoClausesTestCases.cpp
@@ -0,0 +1,49 @@
+#include "catch.hpp"
+using namespace std;
+
+#include "pkb/PKB.h"
+#include "pql/PQLEvaluator.h"
+
+
+PKB pkb = PKB();
+PQLEvaluator pql = PQLEvaluator();
+
+TEST_CASE("No clauses: GetAllVariables") {
+	REQUIRE(pkb.GetDataManager().GetAllVariables() == pql.GetAllSet(TYPE_VAR));
+}
+
+TEST_CASE("No clauses: Procedures") {
+	REQUIRE(pkb.GetDataManager().GetAllProcedures() == pql.GetAllSet(TYPE_PROC));
+}
+
+TEST_CASE("No clauses: Constants") {
+	REQUIRE(pql.ConvertSet(pkb.GetDataManager().GetAllConstants()) == pql.GetAllSet(TYPE_CONST));
+}
+
+TEST_CASE("No clauses: Statements") {
+	REQUIRE(pql.ConvertSet(pkb.GetDataManager().GetAllStatements()) == pql.GetAllSet(TYPE_STMT));
+}
+
+TEST_CASE("No clauses: Statements - Assign") {
+	REQUIRE(pql.ConvertSet(pkb.GetDataManager().GetAllStatements()) == pql.GetAllSet(TYPE_STMT_ASSIGN));
+}
+
+TEST_CASE("No clauses: Statements - Call") {
+	REQUIRE(pql.ConvertSet(pkb.GetDataManager().GetAllStatements()) == pql.GetAllSet(TYPE_STMT_CALL));
+}
+
+TEST_CASE("No clauses: Statements - If") {
+	REQUIRE(pql.ConvertSet(pkb.GetDataManager().GetAllStatements()) == pql.GetAllSet(TYPE_STMT_IF));
+}
+
+TEST_CASE("No clauses: Statements - Print") {
+	REQUIRE(pql.ConvertSet(pkb.GetDataManager().GetAllStatements()) == pql.GetAllSet(TYPE_STMT_PRINT));
+}
+
+TEST_CASE("No clauses: Statements - Read") {
+	REQUIRE(pql.ConvertSet(pkb.GetDataManager().GetAllStatements()) == pql.GetAllSet(TYPE_STMT_READ));
+}
+
+TEST_CASE("No clauses: Statements - While") {
+	REQUIRE(pql.ConvertSet(pkb.GetDataManager().GetAllStatements()) == pql.GetAllSet(TYPE_STMT_WHILE));
+}
