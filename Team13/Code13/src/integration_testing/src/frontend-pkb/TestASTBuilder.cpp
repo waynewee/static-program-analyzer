762b0dd6c8b2132db309709b4f34617488251366
chanjunren <chanjunren@gmail.com>
2020-10-12 15:48:28 +0800

    Fixed Frontend-PKB Integration Testing

--- Team13/Code13/src/integration_testing/src/frontend-pkb/TestASTBuilder.cpp
+++ Team13/Code13/src/integration_testing/src/frontend-pkb/TestASTBuilder.cpp
@@ -3,10 +3,10 @@
 #include <catch.hpp>
 #include <TNode.h>
 #include <queue>
-
+#include <ASTBuilder.h>
 #include "ASTBuilderTestCases.h"
-/*
-SimpleParser parser = SimpleParser();
+
+ASTBuilder astbuilder = ASTBuilder();
 vector<TNode*> InOrderTraversal(TNode* root) {
 
 	std::vector<TNode*> inOrder;
@@ -50,7 +50,7 @@ bool SameAST(vector<TNode*> v1, vector<TNode*> v2) {
 TEST_CASE("Parsing assignment statement for variable to simple expression") {
 	Tokenizer tokenizer(*(AssgnTest1.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(AssgnTest1.root)));
 }
@@ -58,7 +58,7 @@ TEST_CASE("Parsing assignment statement for variable to simple expression") {
 TEST_CASE("Parsing assignment statement for variable to constant") {
 	Tokenizer tokenizer(*(AssgnTest2.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(AssgnTest2.root)));
 }
@@ -66,7 +66,7 @@ TEST_CASE("Parsing assignment statement for variable to constant") {
 TEST_CASE("Parsing assignment statement for variable to variable") {
 	Tokenizer tokenizer(*(AssgnTest3.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(AssgnTest3.root)));
 }
@@ -74,7 +74,7 @@ TEST_CASE("Parsing assignment statement for variable to variable") {
 TEST_CASE("Parsing assignment statement for variable to more complex experssion") {
 	Tokenizer tokenizer(*(AssgnTest4.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(AssgnTest4.root)));
 }
@@ -82,33 +82,15 @@ TEST_CASE("Parsing assignment statement for variable to more complex experssion"
 TEST_CASE("Parsing regular if/else program") {
 	Tokenizer tokenizer(*(IfTestCase1.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(IfTestCase1.root)));
-}
-
-TEST_CASE("Parsing If statement without valid expression") {
-	Tokenizer tokenizer(*(IfTestCase2.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
-}
-
-TEST_CASE("Parsing if statement without else block") {
-	Tokenizer tokenizer(*(IfTestCase3.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
-}
-
-TEST_CASE("Parsing if statement without then keyword") {
-	Tokenizer tokenizer(*(IfTestCase4.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
 }
 
 TEST_CASE("Parsing regular if statement with complex expression program") {
 	Tokenizer tokenizer(*(IfTestCase5.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(IfTestCase5.root)));
 }
@@ -116,86 +98,40 @@ TEST_CASE("Parsing regular if statement with complex expression program") {
 TEST_CASE("Parsing regular while statement") {
 	Tokenizer tokenizer(*(WhileTestCase1.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(WhileTestCase1.root)));
 }
 
-TEST_CASE("Parsing while statement with invalid expression") {
-	Tokenizer tokenizer(*(WhileTestCase2.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
-}
-
-TEST_CASE("Parsing invalid while statement with extra tokens") {
-	Tokenizer tokenizer(*(WhileTestCase3.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
-}
 
 TEST_CASE("Parsing regular while statement with complex expression program") {
 	Tokenizer tokenizer(*(WhileTestCase4.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(WhileTestCase4.root)));
-}
-
-TEST_CASE("Parsing program with extra ; token") {
-	Tokenizer tokenizer(*(ExtraTokensCase1.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
-}
-
-TEST_CASE("Parsing program with extra } token") {
-	Tokenizer tokenizer(*(ExtraTokensCase1.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
-}
-
-TEST_CASE("Parsing program with extra token in definition of procedure") {
-	Tokenizer tokenizer(*(ExtraTokensCase1.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
 }
 
 TEST_CASE("Parsing regular read statement") {
 	Tokenizer tokenizer(*(ValidReadCase1.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(ValidReadCase1.root)));
 }
 
-TEST_CASE("Parsing read statement with wrong spelling") {
-	Tokenizer tokenizer(*(InvalidReadCase1.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
-}
 
 TEST_CASE("Parsing regular print statement") {
 	Tokenizer tokenizer(*(ValidPrintCase1.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(ValidPrintCase1.root)));
-}
-
-TEST_CASE("Parsing print statement with wrong spelling") {
-	Tokenizer tokenizer(*(InvalidPrintCase1.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
 }
 
 TEST_CASE("Parsing call read statement") {
 	Tokenizer tokenizer(*(ValidReadCase1.SourceProgram));
 	tokenizer.Tokenize();
-	TNode* actualRoot = parser.Parse(tokenizer.GetTokenList());
+	TNode* actualRoot = astbuilder.BuildMainPrgNode(tokenizer.GetTokenList());
 
 	REQUIRE(SameAST(InOrderTraversal(actualRoot), InOrderTraversal(ValidReadCase1.root)));
 }
-
-TEST_CASE("Parsing call statement with wrong spelling") {
-	Tokenizer tokenizer(*(InvalidReadCase1.SourceProgram));
-	tokenizer.Tokenize();
-	REQUIRE_THROWS_AS(parser.Parse(tokenizer.GetTokenList()), std::logic_error);
-}*/
