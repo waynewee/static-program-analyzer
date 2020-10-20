df848516f508ea757bc784feb689809ef3d0b89c
jiasimin_ <jas.mint98@gmail.com>
2020-10-20 09:48:04 +0800

    Edited test cases bugs

--- Team13/Code13/tests/Tests13/Others/run_others_tests.ps1
+++
@@ -1,6 +0,0 @@
-Invoke-Expression -Command "autotester.exe 1_source.txt 1_queries.txt xmls/others_1.xml"
-Invoke-Expression -Command "autotester.exe 2_source.txt 2_queries.txt xmls/others_2.xml"
-Invoke-Expression -Command "autotester.exe 3_source.txt 3_queries.txt xmls/others_3.xml"
-Invoke-Expression -Command "autotester.exe 4_source.txt 4_queries.txt xmls/others_4.xml"
-Invoke-Expression -Command "autotester.exe 5_source.txt 5_queries.txt xmls/others_5.xml"
-Invoke-Expression -Command "autotester.exe 6_source.txt 6_queries.txt xmls/others_6.xml"
